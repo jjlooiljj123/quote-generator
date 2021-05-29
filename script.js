4abb95c254e27c7962ab1dd48420f276491fd263
JJLooi <jjlooi.ljj@gmail.com>
2021-05-29 18:41:14 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log('testing');
+const quoteContianer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// show laoding
+function loading() {
+  loader.hidden = false;
+  quoteContianer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContianer.hidden = false;
+  loader.hidden = true;
+}
+
+//show new quote
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = "Uknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// get quotes from api
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // catch error
+  }
+}
+
+// tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//event listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// onLoad
+getQuotes();
